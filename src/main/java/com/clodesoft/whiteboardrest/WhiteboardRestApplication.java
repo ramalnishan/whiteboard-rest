dc71bc93a29ddcec9c278741a7775c5784847e68
ramalnishan <ramal.nishan@hotmail.com>
2023-12-05 16:02:21 +0530

    create whiteboard-rest repo

---
+++ src/main/java/com/clodesoft/whiteboardrest/WhiteboardRestApplication.java
@@ -0,0 +1,13 @@
+package com.clodesoft.whiteboardrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhiteboardRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhiteboardRestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/clodesoft/whiteboardrest/WhiteboardRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clodesoft.whiteboardrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhiteboardRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
